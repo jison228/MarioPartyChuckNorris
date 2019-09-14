536206bd9e524e70b7af5f4fe81c0c9afc1b9597
alanlmerlo <44581540>
2019-09-14 19:00:39 -0300

    First commit

---
+++ src/main/java/com/chucknorris/game/player/PlayerTest.java
@@ -0,0 +1,10 @@
+package com.chucknorris.game.player;
+
+public class PlayerTest {
+    public PlayerTest(boolean value) {
+        if (value) {
+            System.out.println("pepe");
+        } else
+            System.out.println("sarlanga");
+    }
+}
---
+++ src/test/java/com/chucknorris/game/player/PlayerTestTest.java
@@ -0,0 +1,12 @@
+package com.chucknorris.game.player;
+
+import org.junit.Test;
+
+public class PlayerTestTest {
+    @Test
+    public void test() {
+        PlayerTest playerTest = new PlayerTest(true);
+
+        assert true;
+    }
+}
