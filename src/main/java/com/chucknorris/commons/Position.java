9422e37e54db6d85a5bb07bbf8421caed88a6e30
jison228 <jason.linares@outlook.com>
2019-09-22 19:15:36 -0300

    Equals de posicion
    Compara que las posiciones sean iguales bien

--- src/main/java/com/chucknorris/commons/Position.java
+++ src/main/java/com/chucknorris/commons/Position.java
@@ -4,6 +4,31 @@ public class Position {
 	int posX;
 	int posY;
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + posX;
+		result = prime * result + posY;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (posX != other.posX)
+			return false;
+		if (posY != other.posY)
+			return false;
+		return true;
+	}
+
 	public Position(int posX, int posY) {
 		this.posX = posX;
 		this.posY = posY;
