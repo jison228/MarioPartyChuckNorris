0f31fb708f815153d3831d556d4335672bbc86b3
jison228 <jason.linares@outlook.com>
2019-09-22 19:15:54 -0300

    Response Equals
    Compara que las respuestas sean iguales

--- src/main/java/com/chucknorris/mapa/Response.java
+++ src/main/java/com/chucknorris/mapa/Response.java
@@ -10,6 +10,40 @@ public class Response {
 	public ArrayList<Position> availablePositions;
 	public int movs;
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((NodeType == null) ? 0 : NodeType.hashCode());
+		result = prime * result + movs;
+		result = prime * result + ((pos == null) ? 0 : pos.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Response other = (Response) obj;
+		if (NodeType == null) {
+			if (other.NodeType != null)
+				return false;
+		} else if (!NodeType.equals(other.NodeType))
+			return false;
+		if (movs != other.movs)
+			return false;
+		if (pos == null) {
+			if (other.pos != null)
+				return false;
+		} else if (!pos.equals(other.pos))
+			return false;
+		return true;
+	}
+
 	public Response(Position pos, String nodeType, ArrayList<Position> availablePositions, int movs) {
 		super();
 		this.pos = pos;
