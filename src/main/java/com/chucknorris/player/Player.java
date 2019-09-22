f2a2fefeedc6f51448edae2ee15248a1fa91e712
Agustin Di Maulo <dimauloagustin@gmail.com>
2019-09-22 14:58:03 -0300

    Update Player.java
    queria una pos

--- src/main/java/com/chucknorris/player/Player.java
+++ src/main/java/com/chucknorris/player/Player.java
@@ -3,14 +3,16 @@ package com.chucknorris.player;
 public class Player {
 	private String character;
 	private int coins;
-	//private Nodo position; // clase Nodo todavia no creada
+	private int posX;
+	private int posY;
 	
-	public Player(String character, int coins) {//Nodo pos
+	public Player(String character, int coins) {
 		this.character = character;
 		this.coins = coins; 
+		this.posX = 0;
+		this.posY = 0;
 		//despues de avanzar en el proyecto podriamos setear las
 		//monedas en el constructor a un valor fijo como 10 o 0
-		///this.position = pos; 
 		//podriamos setearla aca en una posicion inicial como al principio del tablero
 	}
 	
@@ -26,17 +28,16 @@ public class Player {
 		return this.character;
 	}
 	
-/*	public void setPos(Nodo pos) {
-		this.position = pos; 
+	public void setPos(int x, int y) {
+		this.posX = x; 
+		this.posY = y; 
 	}
-*/ 
 //por si queremos setear al jugador en areas distintas a la seteada en el constructor
 //sin llamar a un metodo del mapa(por ahora solo para ser utilizada en testeos)	
 	
-/*	public Nodo getPos() {
-		return this.position;
+	public int[] getPos() {
+		return new int[] { this.posX, this.posY};
 	}
-*/
 	
 	public String toString() {
 		return this.character + " " + this.coins; //+ Nodo.toString();
