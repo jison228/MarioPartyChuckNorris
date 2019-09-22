7ce84c82b320aaf7263fa83ac8d5ace6fa10a376
Agustin Di Maulo <dimauloagustin@gmail.com>
2019-09-22 16:43:22 -0300

    Update Dice.java

--- src/main/java/com/chucknorris/commons/Dice.java
+++ src/main/java/com/chucknorris/commons/Dice.java
@@ -6,6 +6,11 @@ public class Dice {
 	private int min;
 	private int max;
 	
+	public Dice(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+
 	public int roll(){
 		 Random r = new Random();
 	     return r.nextInt((max - min) + 1) + min;
