11a33b0d54349a5e378ccc06c509f48097c0634f
facutarcaya <facu.tarcaya@gmail.com>
2019-09-24 01:24:13 -0300

    Emprolijando codigo1

--- src/main/java/com/chucknorris/mapa/Node.java
+++ src/main/java/com/chucknorris/mapa/Node.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package com.chucknorris.mapa;
 
 import java.util.ArrayList;
@@ -8,10 +6,6 @@ import java.util.ArrayList;
 import com.chucknorris.commons.Position;
 import com.chucknorris.player.Player;
 
-/**
- * @author agufa
- *
- */
 public abstract class Node {
 	protected ArrayList<Node> next;
 	private Position pos;
@@ -24,12 +18,11 @@ public abstract class Node {
 		return pos;
 	}
 	
-
-
 	public Node(ArrayList<Node> next, Position pos) {
 		this.next = next;
 		this.pos = pos;
 	}
+	
 	public abstract void applyRewards(Player p);
 	public abstract String getType();
 	
