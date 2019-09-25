2fb35ca958299d680003a55596b83a825cf1135a
facutarcaya <facu.tarcaya@gmail.com>
2019-09-25 17:48:39 -0300

    añadido test de setPos de Player

--- src/test/java/com/chucknorris/player/PlayerTest.java
+++ src/test/java/com/chucknorris/player/PlayerTest.java
@@ -1,8 +1,11 @@
 package com.chucknorris.player;
 
 import static org.junit.Assert.assertEquals;
-
+import java.util.ArrayList;
 import org.junit.Test;
+import com.chucknorris.commons.Position;
+import com.chucknorris.mapa.Node;
+import com.chucknorris.mapa.RedNode;
 
 public class PlayerTest {
     @Test
@@ -11,7 +14,13 @@ public class PlayerTest {
         
         test.addCoins(50);
         
+        ArrayList<Node> testNext = null;
+        Node testNode = new RedNode(testNext, new Position(3,5));
+        test.setPos(testNode);
+        
         assertEquals(550,test.getCoins());
         assertEquals("Cristi", test.getCharacter());
+        assertEquals(new Position(3 , 5), test.getPos().getPos());
+        
     }
 }
