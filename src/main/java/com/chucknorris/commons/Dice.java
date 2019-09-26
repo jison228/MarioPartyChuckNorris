0d592e98258d92c4a9cb7a5d23928db14676f7d2
Alan Merlo <alan.lmerlo@gmail.com>
2019-09-25 22:12:00 -0300

    pasamos a test el DiceTest.java y formateamos Dice.java

--- src/main/java/com/chucknorris/commons/Dice.java
+++ src/main/java/com/chucknorris/commons/Dice.java
@@ -3,16 +3,16 @@ package com.chucknorris.commons;
 import java.util.Random;
 
 public class Dice {
-	private int min;
-	private int max;
-	
-	public Dice(int min, int max) {
-		this.min = min;
-		this.max = max;
-	}
+    private int min;
+    private int max;
 
-	public int roll(){
-		 Random r = new Random();
-	     return r.nextInt( ( max - min ) + 1 ) + min;
-	}
+    public Dice(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public int roll() {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
 }
--- src/test/java/com/chucknorris/dice/DiceTest.java
+++ src/test/java/com/chucknorris/dice/DiceTest.java
@@ -1,12 +1,18 @@
 package com.chucknorris.dice;
 
 import com.chucknorris.commons.Dice;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class DiceTest {
-	public static void main(String[] args) {
-		Dice test = new Dice( 1 , 6 );
-		for(int i=0 ; i <6 ; i++) {
-			System.out.println(test.roll());
-		}
-	}
+    @Test
+    public void test_roll_result_beetwen_interval() {
+        Dice test = new Dice(1, 6);
+
+        for (int i = 1; i <= 10000; i++) {
+            int rolled = test.roll();
+
+            Assert.assertTrue(rolled >= 1 && rolled <= 6);
+        }
+    }
 }
