fa82b647efe62cb90be36cddd4c5897442bfd7af
facutarcaya <facu.tarcaya@gmail.com>
2019-09-29 16:22:34 -0300

    Test de Nodo completo

--- src/test/java/com/chucknorris/node/test/NodeTest.java
+++ src/test/java/com/chucknorris/node/test/NodeTest.java
@@ -15,48 +15,47 @@ import static org.junit.Assert.assertEquals;
 
 public class NodeTest {
 
-    Player p;
+	Player p;
 
-    @Before
-    public void executedBeforeEach() {
-        p = new Player("Milei", 0);
-    }
+	@Before
+	public void executedBeforeEach() {
+		p = new Player("Milei", 0);
+	}
 
+	@Test
+	public void nodeConstructorTest() {
+		Node node1 = new YellowNode(null, new Position(3, 3));
+		Node node2 = new RedNode(null, new Position(3, 4));
+		ArrayList<Node> lista = new ArrayList<Node>();
+		lista.add(node1);
+		lista.add(node2);
+		Node nodeTest = new RedNode(lista, new Position(4, 4));
 
-    @Test
-    public void nodeTest() {
-        Node node1 = new YellowNode(null, new Position(3, 3));
-        Node node2 = new RedNode(null, new Position(3, 4));
-        ArrayList<Node> lista = new ArrayList<Node>();
-        lista.add(node1);
-        lista.add(node2);
-        Node nodeTest = new RedNode(lista, new Position(4, 4));
+		assertEquals(nodeTest.getType(), "RED");
+		assertEquals(nodeTest.nextNodes(), lista);
+	}
 
-        assertEquals(nodeTest.getType(), "RED");
-        assertEquals(nodeTest.nextNodes(), lista);
-    }
+	@Test
+	public void redNodeARTest() {
+		Node nodeTest = new RedNode(null, new Position(4, 4));
 
-    @Test
-    public void nodeRedTest() {
-        Node nodeTest = new RedNode(null, new Position(4, 4));
+		nodeTest.applyRewards(p);
+		assertEquals(p.getCoins(), 3);
+	}
 
-        nodeTest.applyRewards(p);
-        assertEquals(p.getCoins(), 3);
-    }
+	@Test
+	public void yellowNodeARTest() {
+		Node nodeTest = new YellowNode(null, new Position(4, 4));
 
-    @Test
-    public void nodeYellowTest() {
-        Node nodeTest = new YellowNode(null, new Position(4, 4));
+		nodeTest.applyRewards(p);
+		assertEquals(p.getCoins(), 10);
+	}
 
-        nodeTest.applyRewards(p);
-        assertEquals(p.getCoins(), 10);
-    }
+	@Test
+	public void whiteNodeARTest() {
+		Node nodeTest = new WhiteNode(null, new Position(4, 4));
 
-    @Test
-    public void nodeWhiteTest() {
-        Node nodeTest = new WhiteNode(null, new Position(4, 4));
-
-        nodeTest.applyRewards(p);
-        assertEquals(p.getCoins(), 0);
-    }
+		nodeTest.applyRewards(p);
+		assertEquals(p.getCoins(), 0);
+	}
 }
