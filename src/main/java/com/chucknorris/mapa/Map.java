aee2dce5fdf91a8e66603a35c9ca834ead7d2315
facutarcaya <facu.tarcaya@gmail.com>
2019-09-29 16:05:41 -0300

    Mejor formateado

--- src/main/java/com/chucknorris/mapa/Map.java
+++ src/main/java/com/chucknorris/mapa/Map.java
@@ -10,79 +10,83 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Map {
-    private HashMap<Position, Node> nodes;
-    private Node start;
+	private HashMap<Position, Node> nodes;
+	private Node start;
 
-    public Map(String tablero) throws FileNotFoundException {
-    	Scanner sc = new Scanner(new File("maps/" + tablero));
-    	this.nodes = new HashMap<Position , Node>();
-    	
-    	int X , Y;
-    	X = sc.nextInt();
-    	Y = sc.nextInt();
-    	Position posEnd = new Position(X , Y);
-    	
-    	EndNode nodeEnd = new EndNode(posEnd);
-    	nodes.put(posEnd , nodeEnd);
-    	
-    	ArrayList<Node> next = null;
-    	Node node = null;
-    	while(sc.hasNext()) {
-    		X = sc.nextInt();
-    		Y = sc.nextInt();
-    		Position pos = new Position(X , Y);
-    		
-    		String type = sc.next();
-    		
-    		String nextChar = sc.next();
-    		next = new ArrayList<Node>();
-    		while(nextChar.charAt(0) != '|') {
-    			X = sc.nextInt();
-    			Y = sc.nextInt();
-    			next.add(this.nodes.get(new Position(X , Y)));
-    			nextChar = sc.next();
-    		}
-    		if(type.equals("RED")) {
-    			node = new RedNode(next , pos);
-    		} else if(type.equals("YELLOW")) {
-    			node = new YellowNode (next , pos);
-    		} else {
-    			node = new WhiteNode(next , pos);
-    		}
-    		nodes.put(pos , node);
-    	}
-    	sc.close();
-    	this.start = node;
-    	nodeEnd.setStart(node);
-    	this.nodes.replace(nodeEnd.getPos(), nodeEnd);
-    }
+	public Map(String tablero) throws FileNotFoundException {
+		Scanner sc = new Scanner(new File("maps/" + tablero));
+		this.nodes = new HashMap<Position, Node>();
 
-    public void initializePlayers(ArrayList<Player> players) {
-        for (Player p : players) {
-            p.setPos(start);
-        }
-    }
+		int X, Y;
+		X = sc.nextInt();
+		Y = sc.nextInt();
+		Position posEnd = new Position(X, Y);
 
-    public int movePlayer(Player p, int movs) {
-        Node node = nodes.get(p.getPos().getPos());
-        int leftMovs = movs;
-        while (node.nextNodes().size() == 1 && leftMovs > 0) {
-            node = node.nextNodes().get(0);
-            leftMovs--;
-        }
-        p.setPos(node);
+		EndNode nodeEnd = new EndNode(posEnd);
+		nodes.put(posEnd, nodeEnd);
 
-        if (leftMovs == 0) {
-        	node.applyRewards(p);
-        }
+		ArrayList<Node> next = null;
+		Node node = null;
+		while (sc.hasNext()) {
+			X = sc.nextInt();
+			Y = sc.nextInt();
+			Position pos = new Position(X, Y);
 
-        return leftMovs;
-    }
+			String type = sc.next();
+			String nextChar = sc.next();
+			
+			next = new ArrayList<Node>();
+			
+			while (nextChar.charAt(0) != '|') {
+				X = sc.nextInt();
+				Y = sc.nextInt();
+				
+				next.add(this.nodes.get(new Position(X, Y)));
+				
+				nextChar = sc.next();
+			}
+			if (type.equals("RED")) {
+				node = new RedNode(next, pos);
+			} else if (type.equals("YELLOW")) {
+				node = new YellowNode(next, pos);
+			} else {
+				node = new WhiteNode(next, pos);
+			}
+			
+			nodes.put(pos, node);
+		}
+		sc.close();
+		this.start = node;
+		nodeEnd.setStart(node);
+		
+		this.nodes.replace(nodeEnd.getPos(), nodeEnd);
+	}
 
-    public int movePlayer(Player p, int movs, Node pos) {
-        p.setPos(pos);
-        return movePlayer(p, movs - 1);
-    }
+	public void initializePlayers(ArrayList<Player> players) {
+		for (Player p : players) {
+			p.setPos(start);
+		}
+	}
 
+	public int movePlayer(Player p, int movs) {
+		Node node = nodes.get(p.getPos().getPos());
+		int leftMovs = movs;
+		while (node.nextNodes().size() == 1 && leftMovs > 0) {
+			node = node.nextNodes().get(0);
+			leftMovs--;
+		}
+		p.setPos(node);
+
+		if (leftMovs == 0) {
+			node.applyRewards(p);
+		}
+
+		return leftMovs;
+	}
+
+	public int movePlayer(Player p, int movs, Node pos) {
+		p.setPos(pos);
+		return movePlayer(p, movs - 1);
+	}
 
 }
