67226e4c306ec632465b287c3c7bd0248c2c80a3
facutarcaya <facu.tarcaya@gmail.com>
2019-10-11 19:55:42 -0300

    añadido segundo pane

--- src/main/java/com/chucknorris/gui/TitleScreen.java
+++ src/main/java/com/chucknorris/gui/TitleScreen.java
@@ -11,6 +11,8 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JEditorPane;
@@ -19,7 +21,8 @@ import javax.swing.JFileChooser;
 
 public class TitleScreen extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel mainPane;
+	private JPanel newGamePane;
 
 	/**
 	 * Launch the application.
@@ -41,68 +44,87 @@ public class TitleScreen extends JFrame {
 	 * Create the frame.
 	 */
 	public TitleScreen() {
+		// JPanel config
 		setResizable(false);
 		setBackground(Color.LIGHT_GRAY);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1280, 720);
-		contentPane = new JPanel();
-		contentPane.setBackground(SystemColor.scrollbar);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
 
-		//Titulo
+		// mainPane config
+		mainPane = new JPanel();
+		mainPane.setBackground(SystemColor.scrollbar);
+		mainPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(mainPane);
+		mainPane.setLayout(null);
+
+		// newGamePane config
+		newGamePane = new JPanel();
+		newGamePane.setBackground(SystemColor.cyan);
+		newGamePane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		newGamePane.setLayout(null);
+
+		// Titulo
 		JLabel Title = new JLabel("INDEFINIDO");
 		int titleW = 280;
 		int titleH = 50;
-		Title.setBounds((getWidth() / 2) - titleW / 2, getHeight() - (int) (getHeight() * 0.90), titleW,
-				titleH);
+		Title.setBounds((getWidth() / 2) - titleW / 2, getHeight() - (int) (getHeight() * 0.90), titleW, titleH);
 		Title.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 42));
 		Title.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(Title);
-		
-		//Botones
+		mainPane.add(Title);
+
+		// Botones
 		int btnW = 400;
 		int btnH = 80;
-		
-		//Nueva Partida
-		JButton btnNewGame = new JButton("Nueva partida");
-		btnNewGame.setBounds((getWidth() / 2) - btnW / 2, getHeight() - (int) (getHeight() * 0.60), btnW,
-				btnH);
-		contentPane.add(btnNewGame);
 
-		
-		//Salir
+		// Nueva Partida
+		JButton btnNewGame = new JButton("Nueva partida");
+		btnNewGame.setBounds((getWidth() / 2) - btnW / 2, getHeight() - (int) (getHeight() * 0.60), btnW, btnH);
+		mainPane.add(btnNewGame);
+
+		// Salir
 		JButton btnQuit = new JButton("Salir");
-		
+
 		btnQuit.setBounds((this.getWidth() / 2) - btnW / 2, this.getHeight() - (int) (this.getHeight() * 0.40), btnW,
 				btnH);
-		contentPane.add(btnQuit);
+		mainPane.add(btnQuit);
 		
-		JLabel lblNombreMapa = new JLabel("NOMBRE MAPA1");
+		//New Game Title
+		JLabel lblNombreMapa = new JLabel("New Game");
 		lblNombreMapa.setFont(new Font("Tahoma", Font.PLAIN, 37));
 		lblNombreMapa.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNombreMapa.setBounds(500, 209, 280, 50);
-		//TODO definir bounds
-		contentPane.add(lblNombreMapa);
+		lblNombreMapa.setBounds((getWidth() / 2) - titleW / 2, getHeight() - (int) (getHeight() * 0.90), titleW, titleH);
+		newGamePane.add(lblNombreMapa);
+
+		// New Game Combo box
+		JComboBox mapCombo = new JComboBox<String>();
+		mapCombo.setFont(new Font("Tahoma",Font.PLAIN,15));
+		mapCombo.setBounds(50, 150, 200, 25);
+		newGamePane.add(mapCombo);
 		
-		//Acciones de los botones
+		// Map image
+		JButton mapImage = new JButton("MAP IMAGE");
+		mapImage.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		mapImage.setHorizontalAlignment(SwingConstants.CENTER);
+		mapImage.setVerticalAlignment(SwingConstants.CENTER);
+		mapImage.setBounds(50,200,300,300);
+		newGamePane.add(mapImage);
+		
+		
+		
+		// Acciones de los botones
 		btnNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				btnNewGame.setVisible(false);
-				btnQuit.setVisible(false);
-				//fileChooser.setVisible(true);
+				mainPane.setVisible(false);
+				setContentPane(newGamePane);
+				// newGamePane.setVisible(true);
 			}
 		});
-		
+
 		btnQuit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		
-		
-		
-		
+
 	}
 }
