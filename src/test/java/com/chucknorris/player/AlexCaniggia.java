89661b19f1b73a161c80aefa76ff729e89a73825
facutarcaya <facu.tarcaya@gmail.com>
2019-10-17 16:12:58 -0300

    Añadido Macri y Alex Caniggia

---
+++ src/test/java/com/chucknorris/player/AlexCaniggia.java
@@ -0,0 +1,32 @@
+package com.chucknorris.player;
+
+import java.util.List;
+
+import com.chucknorris.rewards.GameContext;
+import com.chucknorris.rewards.substractor.pesos.PesosPercentagePlayerSubstractor;
+
+public class AlexCaniggia extends Player {
+	public AlexCaniggia(double pesos,double dolares) {
+		super("Alex Caniggia", pesos, dolares);
+	}
+	
+	public void applyPowerup(List<Player> players, GameContext gameContext) {
+		double pesosP;
+		double dolaresP;
+		
+		for(Player player : players) {
+			player.substractCurrency(new PesosPercentagePlayerSubstractor(50));
+			dolaresP = player.getDolares();
+			pesosP = player.getPesos();
+			player.substractCurrency(new PesosPercentagePlayerSubstractor(100));
+			//player.substractCurrency(new DolaresPercentagePlayerSubstractor(100));
+			player.addDolar(pesosP);
+			player.addPesos(dolaresP);
+		}
+	}
+	
+	public String getPowerupDescription() {
+		return "Resta un 50% de los pesos de todos los jugadores y los cambia con dólares\nSOY HYPER GROSO";
+	}
+	
+}
---
+++ src/test/java/com/chucknorris/player/MacriTest.java
@@ -0,0 +1,44 @@
+package com.chucknorris.player;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MacriTest {
+	Macri gato;
+	Player p1;
+	Player p2;
+	Player p3;
+	List<Player> players;
+	
+	@Before
+	public void executedBeforeEach() {
+		gato = new Macri(15, 100);
+		p1 = new Player("Dummy", 40, 200);
+		p2 = new Player("Dummy", 20, 300);
+		p3 = new Player("Dummy", 25, 100);
+		players = new ArrayList<Player>();
+		players.add(gato);
+		players.add(p1);
+		players.add(p2);
+		players.add(p3);
+	}
+	
+	@Test
+	public void getCharacterTest() {
+		assertEquals("Macri",gato.getCharacter());
+	}
+	
+	@Test
+	public void applyPowerupTest() {
+		gato.applyPowerup(players, null);
+		assertEquals(240, (int)p1.getDolares());
+		assertEquals(360, (int)p2.getDolares());
+		assertEquals(100, (int)p3.getDolares());
+		assertEquals(100, (int)gato.getDolares());
+	}
+}
