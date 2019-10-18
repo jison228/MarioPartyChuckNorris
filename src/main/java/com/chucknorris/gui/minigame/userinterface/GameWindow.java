d8991a4eee98ddf0b95a2df9927a418edc32303d
jison228 <54478268+jison228@users.noreply.github.com>
2019-10-18 02:40:56 -0300

    ventanaprincipal

---
+++ src/main/java/com/chucknorris/gui/minigame/userinterface/GameWindow.java
@@ -0,0 +1,30 @@
+package com.chucknorris.gui.minigame.userinterface;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame {
+	
+	public static final int SCREEN_WIDTH = 800;
+	private GameScreen gameScreen;
+	
+	public GameWindow() {
+		super("Carrera de presis");
+		setSize(SCREEN_WIDTH, 800);
+		setLocation(400, 150);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		
+		gameScreen = new GameScreen();
+		addKeyListener(gameScreen);
+		add(gameScreen);
+	}
+	
+	public void startGame() {
+		setVisible(true);
+		gameScreen.startGame();
+	}
+	
+	public static void main(String args[]) {
+		(new GameWindow()).startGame();
+	}
+}
