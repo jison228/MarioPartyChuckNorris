eeab09f0b46c8553aba331a9015c2f023a503621
facutarcaya <facu.tarcaya@gmail.com>
2019-10-18 00:55:23 -0300

    Añadimos el nodo de BoletaGas
    Ademas añadimos un reward que saque una cantidad fija de pesos

---
+++ src/test/java/com/chucknorris/node/BoletaGasNodeTest.java
@@ -0,0 +1,33 @@
+package com.chucknorris.node;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.chucknorris.commons.Position;
+import com.chucknorris.gamemap.nodes.BoletaGasNode;
+import com.chucknorris.gamemap.nodes.Node;
+import com.chucknorris.player.Player;
+
+public class BoletaGasNodeTest {
+	Player p;
+	Node nodeTest;
+	
+	@Before
+	public void executedBeforeEach() {
+		p = new Player("Cristi", 150, 100);
+		nodeTest = new BoletaGasNode(null, new Position(4, 4));
+	}
+	
+	@Test
+	public void boletaGasRewardTest() {
+		nodeTest.applyReward(p, null, null);
+		assertEquals(p.printWithPesos(), p.getCharacter() + " " + "100.0");
+	}
+
+	@Test
+	public void boletaGasTypeTest() {
+		assertEquals("BG", nodeTest.getType());
+	}
+}
