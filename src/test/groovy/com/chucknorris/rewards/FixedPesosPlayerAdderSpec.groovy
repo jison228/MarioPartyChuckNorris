3d9281512cec65de7c44a0193659924027fe37ba
Alan Merlo <alan.lmerlo@gmail.com>
2019-10-30 20:59:27 -0300

    fixed test

--- src/test/groovy/com/chucknorris/rewards/FixedPesosPlayerAdderSpec.groovy
+++ src/test/groovy/com/chucknorris/rewards/FixedPesosPlayerAdderSpec.groovy
@@ -1,16 +1,32 @@
 package com.chucknorris.rewards
 
 import com.chucknorris.player.Player
+import com.chucknorris.player.PlayerBuilder
 import spock.lang.Specification
 
 class FixedPesosPlayerAdderSpec extends Specification {
 
     void "test fixed pesos addition to single and a list of players, expected only alberto to be affected"() {
         given:
-        Player alberto = new Player("Alberto", 30.0)
-        Player milei = new Player("Milei", 1000.0)
-        Player macri = new Player("Macri", 5000.0)
-        Player cristina = new Player("Cristina", 25.0)
+        Player alberto = new PlayerBuilder()
+                .setCharacter("Alberto")
+                .setPesos(30)
+                .build()
+
+        Player milei = new PlayerBuilder()
+                .setCharacter("Milei")
+                .setPesos(1000)
+                .build()
+
+        Player macri = new PlayerBuilder()
+                .setCharacter("Macri")
+                .setPesos(5000)
+                .build()
+
+        Player cristina = new PlayerBuilder()
+                .setCharacter("Cristina")
+                .setPesos(25)
+                .build()
 
         List<Player> playerList = [
                 milei, macri, cristina, alberto
--- src/test/groovy/com/chucknorris/rewards/substractor/dolar/DolarPercentagePlayersSubstractorSpec.groovy
+++ src/test/groovy/com/chucknorris/rewards/substractor/dolar/DolarPercentagePlayersSubstractorSpec.groovy
@@ -13,19 +13,16 @@ class DolarPercentagePlayersSubstractorSpec extends Specification {
         given:
         Player milei = new PlayerBuilder()
                 .setCharacter("Milei")
-                .setPesos(1000)
                 .setDolar(1000)
                 .build()
 
         Player macri = new PlayerBuilder()
                 .setCharacter("Macri")
-                .setPesos(5000)
                 .setDolar(5000)
                 .build()
 
         Player cristina = new PlayerBuilder()
                 .setCharacter("Cristina")
-                .setPesos(25)
                 .setDolar(25)
                 .build()
 
@@ -46,14 +43,14 @@ class DolarPercentagePlayersSubstractorSpec extends Specification {
         alberto.printWithDolar() == expectedAlberto
 
         where:
-        testCase                                                               | percentage | alberto                        || expectedAlberto  || expectedMilei  || expectedMacri  || expectedCristina
-        "Albert tiene 0 y Resta 10% a todos los players. Alberto gana 602.5"   | 10         | getPlayerWithAlberto()         || "Alberto 602.5"  || "Milei 900.0"  || "Macri 4500.0" || "Cristina 22.5"
-        "Albert tiene 0 y Resta 100% a todos los players. Alberto gana 6025"   | 100        | getPlayerWithAlberto()         || "Alberto 6025.0" || "Milei 0.0"    || "Macri 0.0"    || "Cristina 0.0"
-        "Albert tiene 0 y Resta 0% a todos los players. Alberto gana 6025"     | 0          | getPlayerWithAlberto()         || "Alberto 0.0"    || "Milei 1000.0" || "Macri 5000.0" || "Cristina 25.0"
+        testCase                                                               | percentage | alberto                   || expectedAlberto  || expectedMilei  || expectedMacri  || expectedCristina
+        "Albert tiene 0 y Resta 10% a todos los players. Alberto gana 602.5"   | 10         | getPlayerWithAlberto()    || "Alberto 602.5"  || "Milei 900.0"  || "Macri 4500.0" || "Cristina 22.5"
+        "Albert tiene 0 y Resta 100% a todos los players. Alberto gana 6025"   | 100        | getPlayerWithAlberto()    || "Alberto 6025.0" || "Milei 0.0"    || "Macri 0.0"    || "Cristina 0.0"
+        "Albert tiene 0 y Resta 0% a todos los players. Alberto gana 6025"     | 0          | getPlayerWithAlberto()    || "Alberto 0.0"    || "Milei 1000.0" || "Macri 5000.0" || "Cristina 25.0"
 
-        "Albert tiene 100 y Resta 10% a todos los players. Alberto gana 602.5" | 10         | getPlayerWithAlberto(100, 100) || "Alberto 702.5"  || "Milei 900.0"  || "Macri 4500.0" || "Cristina 22.5"
-        "Albert tiene 100 y Resta 100% a todos los players. Alberto gana 6025" | 100        | getPlayerWithAlberto(100, 100) || "Alberto 6125.0" || "Milei 0.0"    || "Macri 0.0"    || "Cristina 0.0"
-        "Albert tiene 100 y Resta 0% a todos los players. Alberto gana 6025"   | 0          | getPlayerWithAlberto(100, 100) || "Alberto 100.0"  || "Milei 1000.0" || "Macri 5000.0" || "Cristina 25.0"
+        "Albert tiene 100 y Resta 10% a todos los players. Alberto gana 602.5" | 10         | getPlayerWithAlberto(100) || "Alberto 702.5"  || "Milei 900.0"  || "Macri 4500.0" || "Cristina 22.5"
+        "Albert tiene 100 y Resta 100% a todos los players. Alberto gana 6025" | 100        | getPlayerWithAlberto(100) || "Alberto 6125.0" || "Milei 0.0"    || "Macri 0.0"    || "Cristina 0.0"
+        "Albert tiene 100 y Resta 0% a todos los players. Alberto gana 6025"   | 0          | getPlayerWithAlberto(100) || "Alberto 100.0"  || "Milei 1000.0" || "Macri 5000.0" || "Cristina 25.0"
     }
 
     private static Player getPlayerWithAlberto() {
@@ -62,10 +59,9 @@ class DolarPercentagePlayersSubstractorSpec extends Specification {
                 .build()
     }
 
-    private static Player getPlayerWithAlberto(int pesos, int dolares) {
+    private static Player getPlayerWithAlberto(int dolares) {
         return new PlayerBuilder()
                 .setCharacter("Alberto")
-                .setPesos(pesos)
                 .setDolar(dolares)
                 .build()
     }
